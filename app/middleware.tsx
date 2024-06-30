46c6d1aaac904206dda61ef494ef74acdaae411f
khalid <khalid.ibr212@gmail.com>
2024-06-30 18:45:30 +0300

    init | auth setup

--- app/middleware.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-import { useUser } from "@stackframe/stack";
-
-export default function Protected() {
-  useUser({ or: "redirect" });
-  return <h1>You can only see this if you are logged in</h1>;
-}
